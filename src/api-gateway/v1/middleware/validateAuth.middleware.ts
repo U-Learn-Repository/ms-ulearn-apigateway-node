09edaaa6dfe72f5be7875b108207f880b9ef1ed6
fferia <faferiab@unal.edu.co>
2020-05-23 20:59:17 -0500

    Ajuste para solucion de validacion de token

--- src/api-gateway/v1/middleware/validateAuth.middleware.ts
+++ src/api-gateway/v1/middleware/validateAuth.middleware.ts
@@ -3,17 +3,22 @@ import { MiddlewareInterface, ResolverData, NextFn } from "type-graphql";
 import { Context } from "../../../context";
 import { endpoint } from "../endpoint";
 import { ErrorHandler } from "../resolver/error-handler";
+import { isUndefined } from "util";
 
 export class ValidateAuth implements MiddlewareInterface<Context> {
-    async use({ context, info }: ResolverData<Context>, next: NextFn) {
-        console.log('Validate endpoint: ' + info.fieldName);
-        console.log('Validate header: ' + context.headers.authorization);
-        const token = context.headers.authorization || '';
+    async use({ root, args, context, info }: ResolverData<Context>, next: NextFn) {
+        const token = (context as Context).headers.authorization || '';
+        if (!isUndefined(root)) {
+            return next();
+        }
+        console.log('Validar endpoint: ' + info.fieldName);
         try {
-            const data = await axios.get(endpoint.users.autenticacion, {headers: {authorization: token}});
+            console.log('Validar token: ' + token);
+            await axios.get(endpoint.users.autenticacion, { headers: { authorization: token } });
+            console.log('Token valido');
         } catch (error) {
             ErrorHandler.handle(error);
-            throw new Error("Error en la autenticacion"); 
+            throw new Error("Error en la autenticacion");
         }
         return next();
     }
